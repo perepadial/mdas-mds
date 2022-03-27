377839e565925a8fd8ea7a9d3639005cff43194d
Pere Padial <perepadial@gmail.com>
2022-03-27 12:35:49 +0200

    change script

--- .github/deployToOpenshift.sh
+++ .github/deployToOpenshift.sh
@@ -5,7 +5,7 @@ image=$2
 namespace=$3
 
 deploy=`oc get deployment $app_name`
-if [[ "$?" -eq 0 ]]; then
+if [ $? -eq 0 ]; then
     oc set image deployment/$app_name $app_name=$image
     oc rollout restart deployment/$app_name
 else
